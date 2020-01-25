a79dfbec8e7d2999fa874e6c581012f57798a5aa
shaitanshamma <kurbatov_andr@hotmail.com>
2020-01-25 08:24:58 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/testBoot/TestSpringBootApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.testBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/geekbrains/testBoot/TestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.testBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
